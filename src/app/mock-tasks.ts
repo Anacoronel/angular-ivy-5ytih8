426e7e2bb2610089342cb6bfc7d1a5875b016bb2
Anacoronel <anacoronel@hotmail.com>
2021-11-16 16:25:05 -0300

    3 files modified Agregado boton borrar tarea

--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -10,31 +10,31 @@ export const TASKS: Task[] = [
   {
     id: 1,
     text: "Terminar Primer Modulos de Angular",
-    day: "Lunes 22 de noviembre",
+    day: "Lunes 12 de dic",
     reminder:true,
 
   },{
     id: 2,
     text: "Terminar Primer Modulos de Angular",
-    day: "Lunes 22 de noviembre",
+    day: "Lunes 22 de ene",
     reminder:true,
 
   },{
     id: 3,
     text: "Terminar Primer Modulos de Angular",
-    day: "Lunes 22 de noviembre",
+    day: "Lunes 22 de feb",
     reminder:true,
 
   },{
     id: 4,
     text: "Terminar Primer Modulos de Angular",
-    day: "Lunes 22 de noviembre",
+    day: "Lunes 22 de mar",
     reminder:false,
 
   },{
     id: 5,
     text: "Terminar Primer Modulos de Angular",
-    day: "Lunes 22 de noviembre",
+    day: "Lunes 22 de abr",
     reminder:false,
 
   },
