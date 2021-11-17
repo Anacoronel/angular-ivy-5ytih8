9c3e7ed4d929bbfdf464ace16476f8d020dd38a5
Anacoronel <anacoronel@hotmail.com>
2021-11-17 12:00:37 -0300

    2 files modified, 2 added con db json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import {TaskService} from './task.service'
 import { AppComponent } from './app.component';
-
+import {HttpClientModule} from '@angular/common/http';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TasksItemComponent } from './tasks-item/tasks-item.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule],
   declarations: [ AppComponent,  HeaderComponent, ButtonComponent, TasksComponent, TasksItemComponent ],
   bootstrap:    [ AppComponent ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+ providers:[TaskService, ]
 })
 export class AppModule { }
